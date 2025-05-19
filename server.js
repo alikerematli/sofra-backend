c71619db97a8871506539a88ce730b1a9548ad8f
Kerem Atlı <kerematli@Kerem-Mac-mini.local>
2025-05-19 14:13:44 +0300

    CORS ayarı eklendi

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://silver-lollipop-8fe80a.netlify.app'
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
